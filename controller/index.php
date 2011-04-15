5a2511c6b6ae5e63bae4e0aeba3d8fd65991881c
Zach Firestone <zach@fireninewebdesign.com>
2011-04-15 03:20:47 -0400

    Cleaning up a missed file after the merge

--- controller/index.php
+++ controller/index.php
@@ -5,7 +5,6 @@ Class indexController Extends Controller {
 public function index($path) {
 	$this->load->model('index', $path);
 
-	$this->indexModel = new indexModel();
 	$data = $this->indexModel->user_info();
 
 	$this->load->view('index', $path, $data);
