083545607b59a828be36d64ec1947e82c327c8f1
Zach Firestone <zach@fireninewebdesign.com>
2011-04-15 02:04:08 -0400

    Updating how models/views are loaded

--- applications/controller.php
+++ applications/controller.php
@@ -4,6 +4,14 @@ Class Controller {
 
 public $file;
 
+public $load;
+
+public $model;
+
+function __construct() {
+	$this->load = new Load();
+}
+
 function loader($name, $path) {
 	$controllerName = $name . 'Controller';
 	$this->file = $path . '/controller/' . $name . '.php';
--- controller/index.php
+++ controller/index.php
@@ -2,16 +2,14 @@
 
 Class indexController Extends Controller {
 
-public $load;
-
 public function index($path) {
-	$load = new Load();
-	$load->model('index', $path);
+	$this->load->model('index', $path);
 
 	$this->model = new indexModel();
 	$data = $this->model->user_info();
 
-	$load->view('index', $path, $data);
+	$this->load->view('index', $path, $data);
+
 }
 
 }
